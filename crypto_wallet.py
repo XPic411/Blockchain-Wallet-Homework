c8e980eace24262ef70e3254639e08760d42633f
XPic411 <132400410+XPic411@users.noreply.github.com>
2023-10-03 20:19:05 -0400

    2nd commit

--- crypto_wallet.py
+++ crypto_wallet.py
@@ -69,7 +69,7 @@ def send_transaction(w3, account, to, wage):
         "from": account.address,
         "value": value,
         "gas": gasEstimate,
-        "gasPrice": 0,
+        "gasPrice": w3.eth.gasPrice,
         "nonce": w3.eth.getTransactionCount(account.address),
     }
 
